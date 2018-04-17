c2f612e5cef3a7196e0303c670edd16abfcb8308
Henry Doupe <hdoupe@yahoo.com>
2018-04-17 17:55:00 -0400

    Comment out posting for now...

--- dask_rest_api.py
+++ dask_rest_api.py
@@ -54,14 +54,14 @@ async def calc(future, policy_dict):
     print('got aggr_d', aggr_d)
     print('posting result...')
     # posts result to falcon app in mock_pb.py
-    async with aiohttp.ClientSession() as session:
-        async with session.post('http://mock:8000/result', json=json.dumps({'aggr_d': aggr_d})) as resp:
-            status = resp.status
-            text = resp.text()
-    print('local response:')
-    print('\tstatus:', status)
-    text_done = await text
-    print('\tbody:', text_done)
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post('http://mock:8000/result', json=json.dumps({'aggr_d': aggr_d})) as resp:
+    #         status = resp.status
+    #         text = resp.text()
+    # print('local response:')
+    # print('\tstatus:', status)
+    # text_done = await text
+    # print('\tbody:', text_done)
     print('finished. setting result...')
     # set result on future created in `post` to be retrieved in the later
     future.set_result('DONE')
