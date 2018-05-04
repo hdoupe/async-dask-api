c0475ef4d9064c096fa06cd58808017f022ef662
Henry Doupe <hdoupe@yahoo.com>
2018-05-04 11:08:03 -0400

    Read tweak parameter from commandline-otherwise dask remembers prev results

--- test_redis.py
+++ test_redis.py
@@ -1,10 +1,23 @@
 import time
 import requests
 import json
+import sys
+
+# tweak policy dict fromt the command line. otherwise dask will return the
+# saved results
+try:
+    tweak = int(sys.argv[1])
+except Exception as e:
+    print(e)
+    print('setting tweak to 8000')
+    tweak = 8000
+
+kw = {"keywords": {"2018": {"_II_em": [tweak]}}}
 
 IP = 'localhost:8888'
 s = time.time()
-resp = requests.post(f'http://{IP}/taxbrain/', data='{"keywords": {"2018": {"_II_em": [8015]}}}')
+resp = requests.post(f'http://{IP}/taxbrain/',
+                     data=json.dumps(kw))
 f = time.time()
 t = f - s
 print(resp.text)
@@ -26,4 +39,4 @@ while status['status'] not in ('success', 'fail'):
 
     print(f'resp status {t}s', status)
     print('\n\n')
-    time.sleep(0.1)
+    time.sleep(0.25)
